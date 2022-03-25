816baedbbc2f277b570bc2237a6820bfb3687a47
Vedansh Bhardwaj <vedansh.ved@anywhere.co>
2022-03-26 00:32:21 +0530

    Made initial class structure

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to My Splitwise ! \\n Enter 0 -> Existing User \\n Enter 1-> New User ");
+        int choice = sc.nextInt();
+        if (choice == 0) {
+
+        }
+        else if (choice == 1){
+
+        }
+        else {
+            System.out.println("Oops ! You made a wrong choice, Try Again Later");
+        }
+
 
     }
 }
